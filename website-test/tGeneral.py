407e02d77de2ba10ff2df555ab912f5060f2a385
Rob Purser <rpurser@mathworks.com>
2020-11-23 10:51:46 -0500

    Initial automated test of website using selenium

---
+++ website-test/tGeneral.py
@@ -0,0 +1,23 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class TestSnakeGame(unittest.TestCase):
+
+    def test_basic(self):
+        self.browser = webdriver.Chrome()
+        self.addCleanup(self.browser.quit)
+
+        self.browser.get('http://localhost:8080/')
+        assert 'Quite Cool Snake Game by Rob Purser' in self.browser.title
+        
+        section = self.browser.find_element_by_id('titlesection')
+        assert 'not really that cool' in section.text
+        
+        section = self.browser.find_element_by_id('gamesection')
+        assert 'The Destination' in section.text
+        
+        section = self.browser.find_element_by_id('backgroundsection')
+        assert 'The Journey' in section.text
+if __name__ == '__main__':
+    unittest.main()
